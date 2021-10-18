c0af7cd80262108c799624e7544a40f4bc680e30
Taha Shieenavaz <tahashieenavaz@gmail.com>
2021-10-18 17:32:37 +0330

    Code finalized!

--- button.cpp
+++ button.cpp
@@ -1,11 +1,12 @@
+
+
+
 #include <graphics.h>
-
-
 
 #define WIDTH 500 // Canvas width
 #define HEIGHT 500 // Canvas height
 #define FRAMERATE 60
-#define TITLE "My Applicaiton Name"
+#define TITLE "Button Hover Effect - Taha Shieenavaz"
 
 void drawRectangle(int x, int y, int width, int height){
 	rectangle(x-width/2, y-height/2, x+width/2,y+height/2);
@@ -27,33 +28,53 @@ int main(){
 	
 	/* Init */
 	
-	
+	int clickX, clickY;
+	bool mouseKeyPress = false;
 	
 	/* End Init  */
 	
+	// Button meta data
+	
+	int buttonCenterX = WIDTH/2;
+	int buttonCenterY = HEIGHT/2;
+	int buttonWidth = 100;
+	int buttonHeight = 50;
+
+
 	while(true){
 		cleardevice();
-		
-		int buttonCenterX = WIDTH/2;
-		int buttonCenterY = HEIGHT/2;
-		int buttonWidth = 100;
-		int buttonHeight = 50;
-		/* Check Hover */
 	
+		/* Check Hover */
 		if(
 			mousex() <= (buttonCenterX + buttonWidth/2) &&
 			mousex() >= (buttonCenterX - buttonWidth/2) && 
 			mousey() <= (buttonCenterY + buttonHeight/2) &&
 			mousey() >= (buttonCenterY - buttonHeight/2)
 		){
-			setcolor(BLUE);
+			
+			if(ismouseclick(WM_LBUTTONDOWN)){
+				mouseKeyPress = true;
+			}
+			if(ismouseclick(WM_LBUTTONUP)){
+				mouseKeyPress = false;
+			}
+			
+			if(mouseKeyPress){
+				setcolor(RED);
+				system("START https://tahashieenavaz.github.io");
+			}else{
+				clearmouseclick(WM_LBUTTONDOWN);
+				clearmouseclick(WM_LBUTTONUP);
+				setcolor(GREEN);
+			}
+			
 		}else{
 			setcolor(WHITE);
 		}
 		
 		
 		drawRectangle(buttonCenterX,buttonCenterY,buttonWidth,buttonHeight);
-		text(WIDTH/2 - 14,HEIGHT/2 - 7,"Hover Me!");
+		text(WIDTH/2 - 14,HEIGHT/2 - 7,"Click Me!");
 
 
 	
